94cef3794ded13c0ce04b548b8152eada0d53c89
vlad <caraseli02@gmail.com>
2021-05-01 18:42:25 +0200

    start git

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <NavBar />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NavBar from "/src/components/navigation/NavBar.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { NavBar },
+});
+</script>
+
+<style>
+.gradient {
+  background: linear-gradient(to right, #16c0b0, #84cf6a);
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// src/main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/index.css'
+
+createApp(App).mount('#app')
